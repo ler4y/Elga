2ce5a9146228f04e274adef19b8c1872d3884ec1
vboz <vboz@isddesign.com>
2017-04-21 14:37:55 +0300

    Add project files.

---
+++ Elga/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Elga.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Upload()
+        {
+            var httpRequest = HttpContext.Request;
+
+            foreach (string inputTagName in httpRequest.Files)
+            {
+
+                var headers = httpRequest.Headers;
+
+                var file = httpRequest.Files[inputTagName];
+                System.Diagnostics.Debug.WriteLine(file.FileName);
+
+                if (string.IsNullOrEmpty(headers["X-File-Name"]))
+                {
+
+                    // UploadWholeFile(ContentBase, resultList);
+                }
+                else
+                {
+
+                    //UploadPartialFile(headers["X-File-Name"], ContentBase, resultList);
+                }
+
+            }
+            return Json("Ok");
+        }
+    }
+}
---
+++ Elga/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Elga.Startup))]
+namespace Elga
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
