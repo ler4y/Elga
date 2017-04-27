b4f70a20bc6e1762c3ab91fff085c3bbde808b8d
ler4y <ler4y@mail.ru>
2017-04-27 23:41:10 +0300

    add static reconstruction page

--- Elga/App_Start/RouteConfig.cs
+++ Elga/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Elga
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.RouteExistingFiles = true;
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
