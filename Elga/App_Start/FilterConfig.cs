d6f9b96afc251742e566748516792399ef43413b
ler4y <ler4y@mail.ru>
2017-05-09 16:14:10 +0300

    Application insights added

--- Elga/App_Start/FilterConfig.cs
+++ Elga/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Elga
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
